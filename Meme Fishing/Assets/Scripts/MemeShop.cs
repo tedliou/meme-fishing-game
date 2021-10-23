4f217293cb3878bf668a35c28f03a781ad28c220
EntropicEquanimity <64940379+EntropicEquanimity@users.noreply.github.com>
2021-10-23 16:16:58 +0800

    MemeShop

--- Meme Fishing/Assets/Scripts/MemeShop.cs
+++ Meme Fishing/Assets/Scripts/MemeShop.cs
@@ -1,12 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
+[RequireComponent(typeof(AudioSource))]
 public class MemeShop : MonoBehaviour
 {
-    public GameObject[] baitPrefabs;
+    public BaitData[] baits;
+    public Button[] buttons;
     public FishingPole fishingPole;
-    public void SwitchBait()
+    private AudioSource _audioSource;
+
+    private void Start()
+    {
+        _audioSource = GetComponent<AudioSource>();
+
+        for (int i = 0; i < baits.Length; i++)
+        {
+            buttons[i].onClick.AddListener(() => SwitchBait(baits[i]));
+        }
+    }
+    public void SwitchBait(BaitData baitData)
     {
 
     }
