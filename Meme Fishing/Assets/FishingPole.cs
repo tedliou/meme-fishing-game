173153616bb9c25c4e99c923a74af9ff49e443a2
EntropicEquanimity <64940379+EntropicEquanimity@users.noreply.github.com>
2021-10-23 14:32:01 +0800

    Something

--- Meme Fishing/Assets/FishingPole.cs
+++ Meme Fishing/Assets/FishingPole.cs
@@ -1,7 +1,6 @@
 using UnityEngine;
 using DG.Tweening;
 using System.Collections;
-using UnityEngine.UI;
 
 public class FishingPole : MonoBehaviour
 {
@@ -9,24 +8,18 @@ public class FishingPole : MonoBehaviour
     public Transform poleTip;
     public Transform bait;
     public LineRenderer lr;
-    public Image collectionTimer;
 
     public PlayerStats playerStats;
 
     private Rigidbody2D _baitRB;
     public State currentState;
     private Camera cam;
-    private float timeValidZone;
     void Start()
     {
         _baitRB = bait.GetComponent<Rigidbody2D>();
         currentState = State.Throwing;
         cam = Camera.main;
-        cam.transform.position = new Vector3(0, 0, -10);
         cam.orthographicSize = 8;
-        collectionTimer.fillAmount = 0;
-
-        StartPullingBait();
     }
 
     public void ReelIn()
@@ -36,22 +29,14 @@ public class FishingPole : MonoBehaviour
     }
     public void EndFishingState()
     {
-        currentState = State.Standby;
-        StartCoroutine(Delay(1f, () => currentState = State.Throwing));
+        currentState = State.Throwing;
         bait.transform.localPosition = Vector3.zero;
         _baitRB.velocity = Vector2.zero;
         fishingPole.transform.DORotate(new Vector3(0, 0, 75), 0.2f);
-
-        collectionTimer.fillAmount = 0;
     }
     public void StartPullingBait()
     {
         fishingPole.transform.DORotate(new Vector3(0, 0, 75), 0.2f);
-
-        collectionTimer.fillAmount = 0;
-
-        _baitRB.mass = playerStats.mass;
-        _baitRB.gravityScale = playerStats.gravity;
     }
     public void UpdatePullingBait()
     {
@@ -61,9 +46,8 @@ public class FishingPole : MonoBehaviour
     public void EndPullingBait()
     {
         float power = GetLinePulledDistance / playerStats.lineLength * playerStats.stanleyPower;
-        _baitRB.AddForce(-PoleToMouseDir);
+        _baitRB.AddForce(-PoleToMouseDir * power);
         fishingPole.transform.DORotate(new Vector3(0, 0, -45), 0.5f).SetEase(Ease.InFlash);
-        StartCoroutine(Delay(0.5f, () => _baitRB.AddForce(-PoleToMouseDir * power, ForceMode2D.Impulse)));
 
         currentState = State.Standby;
         StartCoroutine(Delay(2f, () => currentState = State.Fishing));
@@ -73,8 +57,6 @@ public class FishingPole : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space)) { EndFishingState(); }
-
         if (currentState == State.Throwing)
         {
             if (Input.GetMouseButtonDown(0))
@@ -92,40 +74,20 @@ public class FishingPole : MonoBehaviour
         }
         else if (currentState == State.Fishing)
         {
-            if (PoleToBaitDist <= 6f)
-            {
-                timeValidZone += Time.deltaTime;
-                collectionTimer.fillAmount = timeValidZone / 2f;
-                if (timeValidZone > 2f)
-                {
-                    EndFishingState();
-                }
-            }
-            else if (timeValidZone > 0)
-            {
-                timeValidZone = Mathf.Max(0, timeValidZone -= Time.deltaTime * 5);
-                collectionTimer.fillAmount = timeValidZone / 2f;
-            }
-
-            if (bait.transform.position.x < -8f)
+            if (PoleToBaitDist <= 1f)
             {
                 EndFishingState();
             }
-
             if (Input.GetMouseButton(1))
             {
                 ReelIn();
             }
-
         }
     }
     private void LateUpdate()
     {
         lr.SetPosition(0, poleTip.position);
         lr.SetPosition(1, bait.position);
-
-        cam.transform.position = Vector3.Lerp((bait.position + fishingPole.transform.position) / 2 + new Vector3(0, 0, -10), cam.transform.position, 0.05f);
-        cam.orthographicSize = Mathf.Clamp((bait.position.x + Mathf.Max(-bait.position.y, 0)) / 2, 6, 20);
     }
     private IEnumerator Delay(float time, System.Action OnComplete)
     {
--- Meme Fishing/Assets/PlayerStats.cs
+++ Meme Fishing/Assets/PlayerStats.cs
@@ -5,14 +5,7 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "Player Stats")]
 public class PlayerStats : ScriptableObject
 {
-    [Header("Stanley")]
     public float reelPower = 1f;
-    public float stanleyPower = 10;
-
-    [Header("Fishing Pole")]
     public float lineLength = 3f;
-
-    [Header("Bait")]
-    public float mass = 0.1f;
-    public float gravity = 1f;
+    public float stanleyPower = 10;
 }
